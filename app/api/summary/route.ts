af926f035f20cea292b5e4d10fd90b5fa02c81b1
james <james@everard.me.uk>
2025-11-15 16:18:39 +0000

    summary route.ts gone

--- app/api/summary/route.ts
+++
@@ -1,3 +0,0 @@
-// This route has been removed. Intentionally left blank to ensure no handlers are exported.
-// Keeping the file temporarily due to tooling limitations that prevent file deletion in this session.
-// The route will effectively return 404.
